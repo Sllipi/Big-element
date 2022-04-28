65cce486d78f7c7d3dc17c9cd48e0d975e4fd694
Sllipi <Sllipi@SLLIPI->
2022-04-28 23:43:09 +0300

    Добавьте файлы проекта.

---
+++ Big element/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Big_element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            byte  randMin = 1;
+            byte  randMax = 10;
+            byte[] bigElement = new byte[30];
+            byte identicalElement = 0;
+            for (byte i = 0; i < bigElement.Length; i++)
+            {
+                bigElement[i] = (byte)rand.Next(randMin, randMax);
+                Console.Write(bigElement[i] + " ");
+            }
+            byte maxElement = bigElement.Max<byte>();
+            for (byte j = 0; j < bigElement.Length; j++)
+            {
+                if (maxElement == bigElement[j])
+                {
+                    identicalElement++;
+                }
+            }
+            Console.WriteLine($"\nНаибольшой элемент = {maxElement}, повторений = {identicalElement}");
+        }
+    }
+}
